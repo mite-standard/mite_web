ca4fac5ab72e249768c09bfe3bdf6a0c6587a493
Mitja M. Zdouc <mmzdouc@gmail.com>
2024-10-27 17:36:57 +0100

    fixed ip

--- mite_web/entrypoint_docker.sh
+++ mite_web/entrypoint_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3 ./prepare_mite_data.py
-gunicorn --worker-class gevent --workers 1 "mite_web:create_app()" --bind "0.0.0.0:8002" # TODO(MMZ 21.10.24): add correct IP
+gunicorn --worker-class gevent --workers 1 "mite_web:create_app()" --bind "0.0.0.0:8004"
