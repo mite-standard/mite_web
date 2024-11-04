68ae5df2dbf8934712d1541f919c4e2b1f3242a6
Mitja M. Zdouc <mmzdouc@gmail.com>
2024-11-04 17:21:04 +0100

    Cleanup htmls

--- mite_web/mite_web/static/js/submission.js
+++ mite_web/mite_web/static/js/submission.js
@@ -27,17 +27,49 @@ function addEnzymeRef() {
 
 function addAuxEnzyme() {
     const container = document.getElementById('aux-enzyme');
+    const index = container.children.length;
     const entryHtml = `
         <div class="aux_enzyme">
-            <div class="row d-flex align-items-center">
-                <div class="col">
-                    <div class="form-floating">
-                        <input type="text" name="enzyme_ref[]" id="enzyme_ref[]" class="form-control" aria-describedby="Enzyme Reference DOI Help"  value="" required>
-                        <label for="enzyme_ref[]" class="form-label">Enzyme Reference DOI</label>
+            <div class="card card-body">
+                <div class="row">
+                    <div class="col">
+                        <h6>Auxiliary Enzyme</h6>
+                    </div>
+                    <div class="col-auto mx-auto">
+                        <button type="button" class="btn btn-danger" onclick="removeEntry(this, '.aux_enzyme')">Remove</button>
                     </div>
                 </div>
-                <div class="col">
-                    <button type="button" class="btn btn-danger" onclick="removeEntry(this, '.aux_enzyme')">Remove</button>
+                <div class="row">
+                    <div class="col">
+                         <div class="form-floating">
+                            <input type="text" name="auxenzyme[${index}]name" id="auxenzyme[${index}]name" class="form-control" aria-describedby="AuxEnzymeNameHelp"  value="" required>
+                            <label for="auxenzyme[${index}]name" class="form-label">Auxiliary Enyzme Name</label>
+                            <div id="AuxEnzymeNameHelp" class="form-text">The common enzyme name (e.g. McjC)</div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-floating">
+                            <input type="text" name="auxenzyme[${index}]description" id="auxenzyme[${index}]description" class="form-control" aria-describedby="AuxEnzymeDescriptionHelp"  value="" required>
+                            <label for="auxenzyme[${index}]description" class="form-label">Auxiliary Enzyme Description</label>
+                            <div id="AuxEnzymeDescriptionHelp" class="form-text">A brief description of the enzyme function</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col">
+                        <div class="form-floating">
+                            <input type="text" name="auxenzyme[${index}]uniprot" id="auxenzyme[${index}]uniprot" class="form-control" aria-describedby="AuxEnzymeUniprotHelp"  value="" required>
+                            <label for="auxenzyme[${index}]uniprot" class="form-label">UniProt/UniParc ID</label>
+                            <div id="AuxEnzymeUniprotHelp" class="form-text">The UniProtKB or UniParc ID</div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-floating">
+                            <input type="text" name="auxenzyme[${index}]genpept" id="auxenzyme[${index}]genpept" class="form-control" aria-describedby="AuxEnzymeGenpeptHelp"  value="" required>
+                            <label for="auxenzyme[${index}]genpept" class="form-label">GenPept ID</label>
+                            <div id="AuxEnzymeGenpeptHelp" class="form-text">The NCBI GenPept ID</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
