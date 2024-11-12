b11fc6924df720dede34c61357573c3c5f7a302e
Mitja M. Zdouc <mmzdouc@gmail.com>
2024-11-12 11:57:23 +0100

    Added script to create issues using github cli

---
+++ mite_web/data_upload.py
@@ -0,0 +1,151 @@
+"""CLI-interface to upload submitted data as Issues on `mite_data`, intended to be used by mite-bot.
+
+This script needs a functioning and authenticated installation of the GitHub CLI.
+
+Copyright (c) 2024-present Mitja Maximilian Zdouc, PhD
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import argparse
+import json
+import subprocess
+import sys
+from pathlib import Path
+
+from pydantic import BaseModel, DirectoryPath
+
+
+class IssueManager(BaseModel):
+    """Methods to upload submitted data as issues to GitHub
+
+    Attributes:
+        src: the location of the data
+        reviewers: the people to assign issues to
+    """
+
+    src: DirectoryPath
+    reviewers: list
+
+    def run(self):
+        """Parses files in the source directory and pushes them to GitHub"""
+
+        for filename in self.src.iterdir():
+            with open(filename) as infile:
+                json_dict = json.load(infile)
+
+            title = ""
+            if json_dict.get("accession") == "MITE9999999":
+                title = f"NEW entry ({filename.stem})"
+            else:
+                title = f"EXISTING entry {json_dict.get("accession")} ({filename.stem})"
+
+            body = f"""
+A submission was performed via the MITE Submission portal.
+Please find the file in the code block below.
+
+## TODO Reviewers
+
+- [x] Automated validation using `mite_extras` performed
+- [ ] Factual correctness of submission checked
+- [ ] Database ID crosslinks verified, added where possible
+- [ ] References properly formatted
+- [ ] Automated validation check ID "BBBBBB..." replaced with real reviewer ID
+
+## Submitted Data
+
+```
+{json.dumps(json_dict, indent=4)}
+```
+"""
+
+            with open(self.src.parent.joinpath("temp").resolve(), "w") as outfile:
+                outfile.write(body)
+
+            try:
+                result = subprocess.run(
+                    [
+                        "gh",
+                        "issue",
+                        "create",
+                        "--repo",
+                        "mite-standard/mite_data",
+                        "--assignee",
+                        f'{",".join(self.reviewers)}',
+                        "--title",
+                        f"{title}",
+                        "--body-file",
+                        f"{self.src.parent.joinpath("temp").resolve()}",
+                        "--label",
+                        "review",
+                    ],
+                    check=True,
+                )
+            except subprocess.CalledProcessError as e:
+                print(f"Error: {e.stderr}")
+
+
+def setup_cli(args: list) -> argparse.Namespace:
+    """Define command line interface options.
+
+    Arguments:
+        args: a list of CLI arguments
+
+    Returns:
+        argparse.Namespace object with command line parameters
+    """
+    parser = argparse.ArgumentParser(
+        description=f"CLI to upload submitted data to mite_data.",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    parser.add_argument(
+        "-i",
+        "--input_dir",
+        type=str,
+        required=True,
+        help="Specifies a directory containing submitted files for processing.",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--reviewers",
+        type=str,
+        nargs="+",
+        required=True,
+        help="Specifies one or more reviewers to assign to issue (separated by whitespace).",
+    )
+
+    return parser.parse_args(args)
+
+
+def main() -> None | SystemExit:
+    """Run main part of code"""
+    args = setup_cli(sys.argv[1:])
+
+    try:
+        issue_manager = IssueManager(src=Path(args.input_dir), reviewers=args.reviewers)
+        issue_manager.run()
+    except Exception as e:
+        print(f"An exception has occurred: {e}")
+        return sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
