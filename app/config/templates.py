65c289d5f835a1ce27b9be980717cd99dd45fb7a
Mitja M. Zdouc <mmzdouc@gmail.com>
2026-01-06 15:54:45 +0100

    Implemented api docs links and display of mite_data version

--- app/config/templates.py
+++ app/config/templates.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
+
+from app.utils.file_handling import load_json
 
 templates = Jinja2Templates(directory="/app/app/templates")
 
@@ -8,3 +12,8 @@ def configure_templates(app: FastAPI):
     """Configure globals in template"""
     templates.env.globals["openapi_url"] = app.docs_url
     templates.env.globals["redoc_url"] = app.redoc_url
+
+    data = load_json(Path("/app/data/version_mite_data.json"))
+    templates.env.globals["version_mite_data"] = data.get(
+        "version_mite_data", "NONE SPECIFIED"
+    )
