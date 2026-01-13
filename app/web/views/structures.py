ec2aa938504ddc12eae52806311eb3dc2f399e51
Mitja M. Zdouc <mmzdouc@gmail.com>
2026-01-13 12:46:24 +0100

    implemented canonicalize

--- app/web/views/structures.py
+++ app/web/views/structures.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Form, Request
 from fastapi.responses import HTMLResponse
 
 from app.core.templates import templates
-from app.schemas.structures import GetStructure, PeptideParams
+from app.schemas.structures import GetStructure, PeptideParams, SmilesParams
 
 router = APIRouter(prefix="/structure", tags=["views"])
 
@@ -30,4 +30,30 @@ async def peptide_query(request: Request, params: Annotated[PeptideParams, Form(
         msg.append(f"An error occurred during peptide smiles generation: {e!s}")
         return templates.TemplateResponse(
             request=request, name="peptide.html", context={"messages": msg}
+        )
+
+
+@router.get("/canonicalize", include_in_schema=False, response_class=HTMLResponse)
+async def canonicalize(request: Request):
+    return templates.TemplateResponse(request=request, name="canonicalize.html")
+
+
+@router.post(
+    "/canonicalize/query", include_in_schema=False, response_class=HTMLResponse
+)
+async def canonicalize_query(request: Request, params: Annotated[SmilesParams, Form()]):
+    msg = []  # TODO: cleanup message
+    try:
+        runner = GetStructure()
+        runner.get_canonical(p=params)
+
+        return templates.TemplateResponse(
+            request=request,
+            name="canonicalize.html",
+            context={"smiles_in": params.smiles, "smiles_out": runner.smiles},
+        )
+    except Exception as e:
+        msg.append(f"An error occurred during peptide smiles generation: {e!s}")
+        return templates.TemplateResponse(
+            request=request, name="canonicalize.html", context={"messages": msg}
         )
