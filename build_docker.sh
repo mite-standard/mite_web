becc3b7d99bbe7171896f35cda88f572f70c537d
Mitja M. Zdouc <mmzdouc@gmail.com>
2026-02-01 21:30:57 +0100

    Expand tags

--- build_docker.sh
+++ build_docker.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 IMAGE="ghcr.io/mite-standard/mite_web"
 
+APP_VERSION="${APP_VERSION:?APP_VERSION not set}"
+
 DATA_VERSION="1.21"
 DATA_RECORD=17998707
 
@@ -12,6 +14,7 @@ EXTRAS_RECORD=17999487
 docker build \
   --build-arg DATA="$DATA_RECORD" \
   --build-arg EXTRAS="$EXTRAS_RECORD" \
-  --tag "$IMAGE:data-$DATA_VERSION-extras-$EXTRAS_VERSION" \
-  --tag "$IMAGE" \
+  --tag "$IMAGE:$APP_VERSION" \
+  --tag "$IMAGE:$APP_VERSION-data-$DATA_VERSION-extras-$EXTRAS_VERSION" \
+  --tag "$IMAGE:latest" \
   .
