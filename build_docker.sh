3ba56c64e70a61120fdb9647e5fd8121b69352e5
Mitja M. Zdouc <mmzdouc@gmail.com>
2026-07-18 20:08:52 +0200

    updated docker file

--- build_docker.sh
+++ build_docker.sh
@@ -6,12 +6,12 @@ IMAGE="ghcr.io/mite-standard/mite_web"
 APP_VERSION="${APP_VERSION:?APP_VERSION not set}"
 
 # mite_data Zenodo: https://doi.org/10.5281/zenodo.13294303
-MITE_DATA_VERSION="1.29"
-MITE_DATA_RECORD=21336549
+MITE_DATA_VERSION="1.30"
+MITE_DATA_RECORD=21432238
 
 # mite_web_extras Zenodo: https://doi.org/10.5281/zenodo.17453501
-MITE_WE_VERSION="1.29"
-MITE_WE_RECORD=21337989
+MITE_WE_VERSION="1.30"
+MITE_WE_RECORD=21432370
 
 docker build \
   --build-arg DATA="$MITE_DATA_RECORD" \
